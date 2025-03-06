97c64d15e0156a9af59f22dbf30dc8b2605b93da
LuKeeeeeeeeeeeeeeeeee <alejocarrer29@gmail.com>
2025-03-06 08:25:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const backendURL = "https://reservas-backend.onrender.com";
+
 document.getElementById("btnAgendar").addEventListener("click", function () {
     document.getElementById("formulario").style.display = "block";
     document.getElementById("reservaciones").style.display = "none";
 });
 
 document.getElementById("btnVer").addEventListener("click", function () {
-    fetch("/leer")
+    fetch(`${backendURL}/leer`)
         .then(response => response.text())
         .then(data => {
             document.getElementById("reservaciones").innerText = data;
@@ -21,13 +23,16 @@ document.getElementById("btnGuardar").addEventListener("click", function () {
 
     let datos = { nombre, fecha: `${fecha} ${hora}`, personas };
 
-    fetch("/guardar", {
+    fetch(`${backendURL}/guardar`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(datos)
     })
     .then(response => response.text())
     .then(data => {
+        document.getElementById("mensaje").innerText = data;
+    });
+});
         document.getElementById("mensaje").innerText = data;
     });
 });
